40a4d4f0045f2f9c2c88b653a2b6a8a8dc7795d4
gaycodegal <sarahmavenoro@gmail.com>
2020-02-06 23:17:53 -0500

    init_rules defined

--- engine/cfg/lang.hh
+++ engine/cfg/lang.hh
@@ -1,9 +1,11 @@
 #pragma once
+#include <cctype>
+#include <iostream>
 #include <string>
 #include <unordered_map>
 #include <vector>
 #include "filter.hh"
-#include "split-whitespace.hh"
+#include "split.hh"
 
 namespace cfg {
 class Lang {
@@ -11,10 +13,16 @@ class Lang {
   std::unordered_map<std::string, std::vector<std::string>> terminals;
   std::unordered_map<std::string, std::vector<std::string>> pairs;
   std::unordered_map<std::string, std::string> words;
-  int gen;
+  int gen = 0;
   std::unordered_map<std::string, std::string> genLookup;
 
  public:
-  int parse_sentence(std::string sentence);
+  int parse_sentence(std::string& sentence);
+  void init_rules(std::string& rules);
+  void parse_rule_value(std::string& rule_type, std::string& value);
+  void new_n_pair_rule(std::string& rule_type, std::vector<std::string>& vals);
+  void new_pair_rule(std::string& rule_type, std::string& k1, std::string& k2);
+  void new_terminal_rule(std::string& rule_type, std::string& k1);
+  std::string next_gen_name(const std::string& rule_type);
 };
 }  // namespace cfg
