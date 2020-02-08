13ea5a9b432468a0e5dae13d5a853661466e2139
gaycodegal <sarahmavenoro@gmail.com>
2020-02-08 17:18:22 -0500

    cleanup

--- engine/cfg/lang.hh
+++ engine/cfg/lang.hh
@@ -5,7 +5,6 @@
 #include <unordered_map>
 #include <vector>
 
-#include "cyk_intermediate.hh"
 #include "filter.hh"
 #include "split.hh"
 
@@ -25,11 +24,6 @@ class Lang {
                      const std::string& k2);
   void new_terminal_rule(const std::string& rule_type, const std::string& k1);
   std::string next_gen_name(const std::string& rule_type);
-  void cyk_add_pairs_to_matrix(
-      std::vector<std::unordered_map<std::string, CYKIntermediate>>& matrix,
-      std::unordered_map<std::string, CYKIntermediate>& left,
-      std::unordered_map<std::string, CYKIntermediate>& right, size_t l_ind,
-      size_t r_ind, size_t insert_ind);
 
  public:
   int parse_sentence(const std::string& sentence);
