a94f44c4a98f24b4fd086679563024716865fadf
gluaxspeed <jonathan.t.pavlik@gmail.com>
2020-03-21 18:21:00 -0400

    fighting using a different delete method for static unique ptr

--- engine/canvas/graphics.cc
+++ engine/canvas/graphics.cc
@@ -1,12 +1,13 @@
 #include "graphics.hh"
 
-// std::unique_ptr<canvas::Graphics, canvas::DestroyGraphics> canvas::Graphics::sInstance = nullptr;
+std::unique_ptr<canvas::Graphics, canvas::DestroyGraphics>
+    canvas::Graphics::sInstance = nullptr;
 bool canvas::Graphics::sInitialized = false;
 
-std::unique_ptr<canvas::Graphics, canvas::DestroyGraphics> canvas::Graphics::Instance(Str title) {
-  if(!sInstance) {
-    // sInstance = std::make_unique<canvas::Graphics>(title);
+/* std::unique_ptr<canvas::Graphics, canvas::DestroyGraphics>
+canvas::Graphics::Instance(Str title) { if(!sInstance) { sInstance =
+std::make_unique<canvas::Graphics>(title);
   }
 
   return sInstance;
-}
+} */
