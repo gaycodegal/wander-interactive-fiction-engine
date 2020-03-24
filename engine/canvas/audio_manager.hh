286856cbdc8b12c50fba542db47ec5f86ed2ca00
gluaxspeed <jonathan.t.pavlik@gmail.com>
2020-03-23 22:00:52 -0400

    forgot to format

--- engine/canvas/audio_manager.hh
+++ engine/canvas/audio_manager.hh
@@ -19,19 +19,27 @@ class AudioManager {
   void inline PlayMusic(Str filename, int loops = -1) {
     Mix_PlayMusic(&this->m_AssetMgr->GetMusic(filename), loops);
   }
-  void inline PauseMusic() { if (Mix_PlayingMusic()) Mix_PauseMusic(); }
-  void inline ResumeMusic() { if (Mix_PausedMusic()) Mix_ResumeMusic(); }
+  void inline PauseMusic() {
+    if (Mix_PlayingMusic()) Mix_PauseMusic();
+  }
+  void inline ResumeMusic() {
+    if (Mix_PausedMusic()) Mix_ResumeMusic();
+  }
 
   void inline PlaySFX(Str filename, int loops = 0, int channel = 0) {
     Mix_PlayChannel(channel, &this->m_AssetMgr->GetSFX(filename), loops);
   }
-	void inline PauseSFX(int channel = 0) { if (Mix_Playing(channel)) Mix_Pause(channel); }
-  void inline ResumeSFX(int channel = 0) { if (Mix_Paused(channel)) Mix_Resume(channel); }
+  void inline PauseSFX(int channel = 0) {
+    if (Mix_Playing(channel)) Mix_Pause(channel);
+  }
+  void inline ResumeSFX(int channel = 0) {
+    if (Mix_Paused(channel)) Mix_Resume(channel);
+  }
 
  private:
   static std::unique_ptr<AudioManager> m_instance;
 
-  AssetManager *m_AssetMgr;
+  AssetManager* m_AssetMgr;
 
   AudioManager();
 };
